acf5c9673b865864cd8b381d3d51f41d4794f9f4
chook <dylanw212@gmail.com>
2020-12-12 10:26:35 +1100

    comment scaffolding

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 
 // What objects do I need?
 
-// Board
+// Board --->
+// An array
+// It's DOM representation?
 
-// Game / Ref
+// Game / Ref --->
+// Roatating players
+// Determining win conditions
 
-// Players
+// Players --->
+// Name, symbol
+// Listeners for humans / logic input for computer
 const playerFactory = (name, symbol) => {
   return { name, symbol };
 };
